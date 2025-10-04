f2ad145332f5514423f44d3fc0be9d288d14f70f
Ryan Vargheese <vargheeseryan@gmail.com>
2025-10-04 22:12:55 +0530

    4th Oct Push

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server: {
-    allowedHosts: [
-      // Add the DNS name of your AWS Load Balancer here
-      'urlloadbalancer-1616105540.ap-south-1.elb.amazonaws.com',
-    ]
-  },
+  plugins: [react()],
 })
